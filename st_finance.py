11a9ac2384421d6b9c0257e2fe5b41fe340d5728
echoopen <3116503100@qq.com>
2022-11-11 23:51:52 +0800

    update

--- st_finance.py
+++ st_finance.py
@@ -11,6 +11,20 @@ with st.sidebar:
     tabs = on_hover_tabs(
         tabName=['金融可视化'],
         iconName=['dashboard'],
+        styles = {'navtab': {'background-color':'#111',
+                            'color': '#818181',
+                            'font-size': '18px',
+                            'transition': '.3s',
+                            'white-space': 'nowrap',
+                            'text-transform': 'uppercase'},
+                'tabOptionsStyle': {':hover :hover': {'color': 'red',
+                                                'cursor': 'pointer'}},
+                'iconStyle':{'position':'fixed',
+                            'left':'4.5px',
+                            'text-align': 'left'},
+                'tabStyle' : {'list-style-type': 'none',
+                                'margin-bottom': '30px',
+                                'padding-left': '5px'}},
         default_choice=0
         )
 
